70218263f7b269eabd6696bca025a29e5a00e552
futao <1185172056@qq.com>
2020-10-28 15:45:09 +0800

    123

--- src/main/java/com/futao/starter/fustack/StarterFustackApplication.java
+++ src/main/java/com/futao/starter/fustack/StarterFustackApplication.java
@@ -1,8 +1,14 @@
 package com.futao.starter.fustack;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author futao
+ * @date 2020/10/28
+ */
+@EnableSwagger2Doc
 @SpringBootApplication
 public class StarterFustackApplication {
 
